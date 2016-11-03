a4f6613dba2e58fed68f1759449a9e2bf8ccc2af
Emeka Ezeji-Okoye <cherrypickinassets@gmail.com>
2016-11-03 18:24:12 -0500

    Included refs

---
+++ Pods/Target Support Files/Pods-PettyCash/Pods-PettyCash-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PettyCash : NSObject
+@end
+@implementation PodsDummy_Pods_PettyCash
+@end
---
+++ Pods/Target Support Files/Pods-PettyCashTests/Pods-PettyCashTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_PettyCashTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PettyCashTestsVersionString[];
+
